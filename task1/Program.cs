386e03e93ae02815e00d82d849731cd0c2992603
Dreadnout <rfiflaa@gmail.com>
2023-10-20 23:00:41 +0300

    task1

---
+++ task1/Program.cs
@@ -0,0 +1,19 @@
+﻿System.Console.WriteLine("Введите первое число число");
+
+int inputData0 = int.Parse(Console.ReadLine());
+
+System.Console.WriteLine("Введите второе число");
+
+int inputData1 = int.Parse(Console.ReadLine());
+
+if (inputData1 == inputData0 * inputData0)
+{
+    System.Console.WriteLine("Yes");
+
+}
+
+else System.Console.WriteLine("No");
+
+
+
+
