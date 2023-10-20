d788845c00af5e75af7083b7153b877c946eb4f6
Dreadnout <rfiflaa@gmail.com>
2023-10-20 22:39:31 +0300

    TASK0

---
+++ task0/Program.cs
@@ -0,0 +1,15 @@
+﻿//Напишите программу, которая на вход принимает число и выдаёт его квадрат (число умноженное на само себя).
+
+//Например:
+//4 -> 16 
+//-3 -> 9 
+//-7 -> 49
+
+Console.WriteLine("Введите число");
+
+int inputData = int.Parse(Console.ReadLine());
+
+int result = inputData * inputData;
+
+Console.Write($"Корень числа {inputData}  равен  {result}");
+
